1feb7b1de8e26ff0a3e91b4ad4219612a766e99c
PaulCamara5677 <144330112+PaulCamara5677@users.noreply.github.com>
2023-10-14 21:36:42 +0400

    chapter_2_exercise4.py

---
+++ Chapter 2- Variables & Comments/Exercises/exercise4.py
@@ -0,0 +1,9 @@
+#Use a variable to represent your favorite number. 
+##Then,using that variable, create a message that reveals your 
+###favorite number. Print
+####that message.
+
+favorite_number = "76"
+input ("Do you wish to know my favorite number? Enter 'yes' to continue")
+print ("Paul's favorite number is...", favorite_number)
+ 
