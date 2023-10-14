4b96f76774fd623b7d59b4f7b3cf96c2eb711a87
PaulCamara5677 <144330112+PaulCamara5677@users.noreply.github.com>
2023-10-14 21:36:33 +0400

    chapter_2_exercise3.py

---
+++ Chapter 2- Variables & Comments/Exercises/exercise3.py
@@ -0,0 +1,14 @@
+#Exercise 3
+'''Use a variable to represent a person’s name, and include some whitespace 
+characters at the beginning and end of the name. 
+Make sure you use each character combination, “\t” and “\n”, at least once.'''
+
+
+
+
+#Answer
+name="\tPaul Yram\n"
+print (name)
+print (name.lstrip())
+print (name.rstrip())
+print (name.strip())
