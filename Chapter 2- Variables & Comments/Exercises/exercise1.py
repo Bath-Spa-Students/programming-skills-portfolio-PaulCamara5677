bfb0ef4801c5fce6bab0d3e3f984dc00fe8471e2
PaulCamara5677 <144330112+PaulCamara5677@users.noreply.github.com>
2023-10-14 21:35:40 +0400

    chapter_2_exercise1.py

---
+++ Chapter 2- Variables & Comments/Exercises/exercise1.py
@@ -0,0 +1,9 @@
+#Assign a message to a variable, and print that message. 
+##Then change the value of the variable to a new message, and print 
+###the new message.
+
+
+greeting = "Hello there."
+print (greeting)
+greeting = "A Pleasure to see you today."
+print (greeting)
