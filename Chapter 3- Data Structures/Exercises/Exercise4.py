55edf623ae1c442a741d9a7edd030744ab8a1604
PaulCamara5677 <144330112+PaulCamara5677@users.noreply.github.com>
2023-10-22 21:32:33 +0400

    Chapter_3_exercise4.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise4.py
@@ -0,0 +1,17 @@
+## Exercise 4: Guest List :ballot_box_with_check:
+
+guests_list = ['Saher','Guinivere','Katie','Skyler']
+
+name = guests_list[0].title()
+print(f"{name}, come to my house and eat dinner with me tonight.")
+
+name = guests_list[1].title()
+print(f"{name}, come to my house and eat dinner with me tonight.")
+
+name = guests_list[2].title()
+print(f"{name}, come to my house and eat dinner with me tonight.")
+
+name = guests_list[3].title()
+print(f"{name}, come to my house and eat dinner with me tonight.")
+
+#  Guinivere could not make it to dinner! Let's invite Elise instead.
