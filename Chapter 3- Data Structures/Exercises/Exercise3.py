6aba882aae2e790a9ee9d35c6dfe3de1b8f4d01b
PaulCamara5677 <144330112+PaulCamara5677@users.noreply.github.com>
2023-10-22 21:32:22 +0400

    Chapter_3_exercise3.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise3.py
@@ -0,0 +1,12 @@
+## Exercise 3: Your Own List :ballot_box_with_check:
+
+Transportation = ['Camaro','Harley Davidson','Private plane']
+
+message = f"I would like to own a {Transportation[0]}, and explore with it everyday"
+print(message)
+
+message = f"I would like to own a {Transportation[1]}, and go for a casual ride with it."
+print(message)
+
+message = f"I would like to own a {Transportation[2]}, and travel the world."
+print(message)
