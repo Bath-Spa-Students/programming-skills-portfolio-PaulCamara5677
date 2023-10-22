b15003a77f24d0109bc7eb228852f5a903826a1e
PaulCamara5677 <144330112+PaulCamara5677@users.noreply.github.com>
2023-10-22 21:32:57 +0400

    Chapter_3_exercise7.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise7.py
@@ -0,0 +1,32 @@
+##Think of at least five places in the world you’d like to visit.
+###Store the locations in a list. Make sure the list is not in alphabetical order.
+
+Places_to_Visit= ['Rome, Italy','Sydney, Australia','Amsterdam, Netherlands','Tokyo, Japan','Barcelona, Spain']
+
+print("\nOriginal order:" )
+print(sorted(Places_to_Visit))
+
+print("\nAlphabetical order:" )
+print(sorted(Places_to_Visit))
+
+print("\nReverse Alphabetical order:" )
+print(sorted(Places_to_Visit,reverse=True))
+
+print("\nOriginal order:" )
+print(Places_to_Visit )
+
+print("\nReverse order:" )
+Places_to_Visit.reverse()
+print(Places_to_Visit)
+
+print("\nOrginal order:" )
+Places_to_Visit.reverse()
+print(Places_to_Visit)
+
+print("\nAlphabetical order")
+Places_to_Visit .sort()
+print(Places_to_Visit)
+
+print("\nReverse Alphabetical order:" )
+Places_to_Visit.sort(reverse=True)
+print(Places_to_Visit)
