ca822d24b248aa5d33a83987396ed6feb20c636f
PaulCamara5677 <144330112+PaulCamara5677@users.noreply.github.com>
2023-10-22 21:32:11 +0400

    Chapter_3_exercise2.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise2.py
@@ -0,0 +1,15 @@
+## Exercise 2: Greetings :ballot_box_with_check:
+
+friends = ['Isaac','Wayeth','Rafael','Yyan']
+
+message = f"Hello {friends[0]}, Good morning to you."
+print = message
+
+message = f"Hello {friends[1]}, Good afternoon to you."
+print = message
+
+message = f"Hello {friends[2]}, Good evening to you."
+print = message
+
+message = f"Hello {friends[3]}, Good blessings to you."
+print = message
