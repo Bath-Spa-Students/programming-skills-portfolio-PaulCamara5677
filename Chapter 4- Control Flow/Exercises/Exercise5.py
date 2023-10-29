d1d507a8d4c922f47e3827b2514e24874e84e455
PaulCamara5677 <144330112+PaulCamara5677@users.noreply.github.com>
2023-10-29 17:06:55 +0400

    ch4-exercise5.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise5.py
@@ -0,0 +1,17 @@
+#5 if-statements
+
+favorite_fruits = ("Orange","Apple","Raspberry","Blueberry","Pineapple")
+if "Raspberry" in favorite_fruits:
+    print("I really like Raspberry.")
+
+if "Orange" in favorite_fruits:
+    print("I really like Orange.")
+
+if "Pineapple" in favorite_fruits:
+    print("I really like Pineapple.")
+
+if "Blueberry" in favorite_fruits:
+    print("I really like Blueberry.")
+
+if "Apple" in favorite_fruits:
+    print("I really like Apple.")
