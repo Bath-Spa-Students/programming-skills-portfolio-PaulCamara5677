e9db6bb0aa6f52d3db7bdb562729642adcd95f19
PaulCamara5677 <144330112+PaulCamara5677@users.noreply.github.com>
2023-10-29 17:06:10 +0400

    ch4-exercise2.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise2.py
@@ -0,0 +1,14 @@
+#If block statement
+
+alien_color = "green"
+if alien_color == "green":
+    print("You have scored 5 points for shooting the green alien.")
+else:
+    print("You have scored 10 points for shooting the bad alien.")
+
+#Else block statement
+alien_color = "red"
+if alien_color == "green":
+    print("You have scored 5 points for shooting the green alien.")
+else:
+    print("You have scored 10 points for shooting the bad alien.")
