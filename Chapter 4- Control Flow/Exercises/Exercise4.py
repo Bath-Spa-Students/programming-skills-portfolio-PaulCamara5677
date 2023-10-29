7c244559154779167f2b90b1ba877b634f336d08
PaulCamara5677 <144330112+PaulCamara5677@users.noreply.github.com>
2023-10-29 17:06:45 +0400

    ch4-exercise4.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise4.py
@@ -0,0 +1,18 @@
+#if-elif-else chain 
+
+age = 1
+
+if age < 2:
+    print("You are a Baby.")
+elif age <4:
+    print("You are a young child.")
+elif age <14:
+    print("You are still a kid.")
+elif age <20:
+    print("You are now a teenager.")
+elif age <29:
+    print("You are now a young adult.")
+elif age <75:
+    print("You are now an Adult.")
+else: 
+    print("You are now an elderly person.")
