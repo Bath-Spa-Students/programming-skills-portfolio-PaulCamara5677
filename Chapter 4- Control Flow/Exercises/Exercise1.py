cccdbd9575f4a7d0f6c806908c015f0a09080e6f
PaulCamara5677 <144330112+PaulCamara5677@users.noreply.github.com>
2023-10-29 17:05:53 +0400

    ch4-exercise1.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise1.py
@@ -0,0 +1,12 @@
+Sales = float(input("Enter Price :"))
+revenue = 50000
+print("Your revenue is : "+str(revenue))
+
+if Sales > 10000 :
+    revenue = 5000
+    print('Your revenue is : '+str(revenue ) )
+
+else:
+    revenue = 5000
+    print('Your revenue is : '+str(revenue ) )
+
