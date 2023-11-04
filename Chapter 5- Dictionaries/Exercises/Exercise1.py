7c7c9315a214afddf31e6ba3c5a0f0d8824e7944
PaulCamara5677 <144330112+PaulCamara5677@users.noreply.github.com>
2023-11-04 15:35:34 +0400

    Ch5-ex1-Student_infro-dictionary.py

---
+++ Chapter 5- Dictionaries/Exercises/Exercise1.py
@@ -0,0 +1,23 @@
+dictionary = {'first_name' : 'Paul Yram',
+              'last_name' : 'Camara',
+              'age' : '21',
+              'city' : 'Al Nahda, Sharjah' }
+print( dictionary.get ("first_name") )
+
+dictionary = {'first_name' : 'Paul Yram',
+              'last_name' : 'Camara',
+              'age' : '21',
+              'city' : 'Al Nahda, Sharjah' }
+print( dictionary.get ("last_name") )
+
+dictionary = {'first_name' : 'Paul Yram',
+              'last_name' : 'Camara',
+              'age' : '21',
+              'city' : 'Al Nahda, Sharjah' }
+print( dictionary.get ("age") )
+
+dictionary = {'first_name' : 'Paul Yram',
+              'last_name' : 'Camara',
+              'age' : '21',
+              'city' : 'Al Nahda, Sharjah' }
+print( dictionary.get ("city") )
