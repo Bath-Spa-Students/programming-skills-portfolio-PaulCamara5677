cc7f3545652ec6118627f46a93fc8700ae5f54bd
PaulCamara5677 <144330112+PaulCamara5677@users.noreply.github.com>
2023-11-04 15:35:55 +0400

    Ch5-ex2-glossary.py

---
+++ Chapter 5- Dictionaries/Exercises/Exercise2.py
@@ -0,0 +1,49 @@
+#keyword variable 
+
+Glossary = {'keyword' : 'variable',
+               'pronunciation' : 'veuh ree uh bl', 
+              'synonyms' : 'changeable, inconstant, editable, differed',
+              'definition' : 'Python variables contains the value of the stored data.' }
+
+for word, definition in Glossary.items():
+    print(f'{word}:\n {definition}')
+
+#keyword tuple
+
+Glossary = {'keyword' : 'tuple',
+              'pronunciation' : 'tuh pl', 
+              'synonyms' : 'record, row, list, attribute',
+              'definition' : 'Python tuples are a type of data structure that is very similar to lists.' }
+
+for word, definition in Glossary.items():
+    print(f'{word}:\n {definition}')
+
+#keyword operator
+
+Glossary = {'keyword' : 'Operator',
+              'pronunciation' : 'o puh ray tuh',
+              'synonyms' : 'director, manager, contractor, user', 
+              'definition' : 'An operator in Python is used to define variables, functions, and classes.' }
+
+for word, definition in Glossary.items():
+    print(f'{word}:\n {definition}')
+
+#keyword repository
+
+Glossary = {'keyword' : 'repository',
+              'pronunciation' : 'ruh po zuh tuh ree',
+              'synonyms' : 'archive, storehouse, safe, depot', 
+              'definition' : 'Python repositories are where software development teams that develop Python share their code artifacts.' }
+
+for word, definition in Glossary.items():
+    print(f'{word}:\n {definition}')
+
+#keyword
+
+Glossary =  {'keyword' : 'commit',
+              'pronunciation' : 'kuh mit',
+              'synonyms' : 'execute, fulfill, accomplish, send', 
+              'definition' : 'This method lets an operator save all the changes made in a database.' }
+
+for word, definition in Glossary.items():
+    print(f'{word}:\n {definition}')
