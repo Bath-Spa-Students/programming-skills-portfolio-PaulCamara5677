ebd77aa62016add8797ed6f3f7bec112f5421e1b
PaulCamara5677 <144330112+PaulCamara5677@users.noreply.github.com>
2023-11-10 16:12:16 +0400

    Chapter6-Exercise3.py

---
+++ Chapter 6- Loops/Exercises/Exercise3.py
@@ -0,0 +1,12 @@
+### Exercise 3: Infinity :ballot_box_with_check:
+#Write a loop that never ends, and run it. (To end the loop, press ctrl-C or close the window displaying the output.)
+#Infinite loop?
+
+counter = 0 
+while True:
+    counter +=1
+    if counter % 2 == 0:
+        print("Even number:", counter)
+    else:
+        print("Odd number:", counter)
+    #This exercise will result in an ininfinite loop only stopped by either closing Video Studio code or by deleting the window in the terminal panel.
