8ba119545265fcbae7f22c922d8d8e3872f42944
PaulCamara5677 <144330112+PaulCamara5677@users.noreply.github.com>
2023-11-11 14:23:50 +0400

    Ch7-Exercise1.py

---
+++ Chapter 7- Functions/Exercise1.py
@@ -0,0 +1,6 @@
+#write a function called display_message() that will print one sentence telling the everyone what you are showing
+##function and make sure the message displays correctly.
+def display_message() :
+    print("I am learning the necessary knowledge and terms for this chapter.")
+
+display_message()
