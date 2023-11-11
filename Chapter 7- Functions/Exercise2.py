acda3904653561df6977ce723e6620cf35286449
PaulCamara5677 <144330112+PaulCamara5677@users.noreply.github.com>
2023-11-11 14:53:34 +0400

    Chapter7-Exercise2.py

---
+++ Chapter 7- Functions/Exercise2.py
@@ -0,0 +1,8 @@
+#Declaring the student's favorite book using the def function
+def favorite_book(title):
+#def statement as displayed above.
+    print("My favorite book is" + title)
+#the print will declare how the variable is valued by the student.
+favorite_book(" War and Peace by Russian author Leo Tolstoy.")
+#Note: I had to add the Author's pen name as I realized from searching
+#the old book's title had many different publications but I prefer the one by the author Leo Tolstoy.
