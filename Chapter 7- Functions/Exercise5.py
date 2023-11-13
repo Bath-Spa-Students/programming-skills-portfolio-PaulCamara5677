04d4475ed84ccfcdb982a981c84b76f896c56ab8
PaulCamara5677 <144330112+PaulCamara5677@users.noreply.github.com>
2023-11-13 10:31:01 +0400

    Ch7-Exercise5.py

---
+++ Chapter 7- Functions/Exercise5.py
@@ -0,0 +1,11 @@
+#Defining a city's country of origin or which country the city is located in.
+def describe_city(city, country = ' UAE'):
+#print will display a string in between the city and country value in order to make it clear where is where.
+    print(city + "is located in" + country)
+#Three examples as stated below
+describe_city('Cabanatuan City', ' Philippines')
+#Cabanatuan city is located in Philippines
+describe_city('Dubai ')
+#Dubai is located in UAE
+describe_city('Ras Al-Khaimah ')
+#Ras Al-Khaimah is located in UAE
