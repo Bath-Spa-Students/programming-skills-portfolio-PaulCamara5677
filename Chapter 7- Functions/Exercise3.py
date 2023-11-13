22389ed27ce9a33b39d81d0b807ad29f22da2794
PaulCamara5677 <144330112+PaulCamara5677@users.noreply.github.com>
2023-11-13 10:27:05 +0400

    Ch7-Exercise3.py

---
+++ Chapter 7- Functions/Exercise3.py
@@ -0,0 +1,9 @@
+#Declaring a shirt's size and printed design
+def make_shirt(size,message):
+#print will allow the user to create a statement to emphasize the size of the shirt and its printed message in make_shirt.
+    print(f"The shirt's size is {size} and the message printed on the design is: {message}")
+#the make_shirt variable will define the size and message for the shirt.
+make_shirt("Large", "Group 2 Homies!")
+#I dedicate this shirt to my good friends, Isaac Gerard, Yyan Flores, Gabriel Jude, 
+# Wayeth Ken, Rafael Saimon, Raphael Chester, Joseph Gianne & Aviona.  
+make_shirt(size = "small", message = "Group 2 Dudes!")
