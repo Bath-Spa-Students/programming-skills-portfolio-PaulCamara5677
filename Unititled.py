9e378c6ca114200354233fbe6d08dd5c63f44cd9
PaulCamara5677 <144330112+PaulCamara5677@users.noreply.github.com>
2023-11-15 17:38:01 +0400

    Untitled-practice.py

---
+++ Unititled.py
@@ -0,0 +1,16 @@
+f_name= "Paul Yram "
+l_name= "Camara"
+name = f_name + l_name
+print (name, end ="")
+print(" is a Bad Man.")
+
+print ("\n \t well you done done me and you bet I felt it",
+"\n \t I tried to be chill but youre so hot that I melted,"+
+"\n \t I fell right through the cracks,"+
+"\n \t now Im trying to get back,")
+
+print (f'\n {f_name} is currently recording a new video ')
+print(f'\t {l_name}  is a proud line of men and women that are loyal')
+
+val = (69000)
+print (f'\n \t The Value of { val + 420}')
