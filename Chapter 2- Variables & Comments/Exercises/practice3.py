70ab385726e7cf9b40d3497c6f9a981e0817339d
PaulCamara5677 <144330112+PaulCamara5677@users.noreply.github.com>
2023-11-15 22:30:02 +0400

    Ch2-practice3.py

---
+++ Chapter 2- Variables & Comments/Exercises/practice3.py
@@ -0,0 +1,12 @@
+#Write a python program that takes an input 5 from user and then type cast those values to string, int
+#and float in the separate variables. Print all the variables.
+
+num = input("Please enter a number:")
+
+str = str(num)
+int = int(num)
+float = float(num)
+
+print(f"String:{num}")
+print(f"Integer:{num}")
+print(f"Float:{float}")
