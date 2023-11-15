bddbb90730e38219fff27a3d1ef0e827854c300c
PaulCamara5677 <144330112+PaulCamara5677@users.noreply.github.com>
2023-11-15 21:30:06 +0400

    Ch6-practice3.py

---
+++ Chapter 6- Loops/Exercises/practice3.py
@@ -0,0 +1,9 @@
+#Writing a Python program that will use a loop in order to find the sum of all numbers from 1 through 10.
+
+sum=0
+
+#The range code below will loop to find the overall sum of numbers.
+
+for num in range(1, 101):
+    sum +=num
+    print("The Sum of all number is:",sum)
