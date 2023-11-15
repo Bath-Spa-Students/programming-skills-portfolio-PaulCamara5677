410163a1b534da334ab0de367cb9358329744e0b
PaulCamara5677 <144330112+PaulCamara5677@users.noreply.github.com>
2023-11-15 22:05:41 +0400

    Ch2-practice1.py

---
+++ Chapter 2- Variables & Comments/Exercises/practice1.py
@@ -0,0 +1,10 @@
+#Q1: Write a python code that uses three variables x, y, and z with respective values 10,20 and 30. Add all the 
+#variables and then store the result in variable a. Print the value of a. 
+
+x = 10
+y = 20
+z = 30
+
+#The code shown below will print the addition of x, y, and z.
+
+print(x+y+z)
