14e40de3be944a410f26952e713fcac54082cf33
PaulCamara5677 <144330112+PaulCamara5677@users.noreply.github.com>
2023-11-15 17:10:43 +0400

    Ch3-Ex2-edited.py

--- Chapter 3- Data Structures/Exercises/Exercise2.py
+++ Chapter 3- Data Structures/Exercises/Exercise2.py
@@ -3,13 +3,13 @@
 friends = ['Isaac','Wayeth','Rafael','Yyan']
 
 message = f"Hello {friends[0]}, Good morning to you."
-print = message
+print(message)
 
 message = f"Hello {friends[1]}, Good afternoon to you."
-print = message
+print(message)
 
 message = f"Hello {friends[2]}, Good evening to you."
-print = message
+print(message)
 
 message = f"Hello {friends[3]}, Good blessings to you."
-print = message
+print(message)
