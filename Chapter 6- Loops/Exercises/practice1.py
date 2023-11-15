1863cbefab90df429129e251a6f0351149fe9724
PaulCamara5677 <144330112+PaulCamara5677@users.noreply.github.com>
2023-11-15 17:37:13 +0400

    Ch6-practice-1.py

---
+++ Chapter 6- Loops/Exercises/practice1.py
@@ -0,0 +1,10 @@
+#Count +5
+for x in range (0, 20, 5):
+    print(x, end=",")
+
+#Activity : Calculate the square root of each number in a range:
+
+N= int (input("Enter max number for range :"))
+for x in range (0, 20, 5):
+    print(x,end=",")
+
