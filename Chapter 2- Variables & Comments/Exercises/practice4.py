b597944a61cc082b8098c71cfc39307d154739cf
PaulCamara5677 <144330112+PaulCamara5677@users.noreply.github.com>
2023-11-15 22:38:44 +0400

    Ch2-practice4.py

---
+++ Chapter 2- Variables & Comments/Exercises/practice4.py
@@ -0,0 +1,7 @@
+#Q4 Write a python program that stores an integer and string value to variables x and y. Print the type of variable x and y.
+
+x = 100
+y = "Hello there, my name is Paul Yram Bartolome Camara but you may call me Paul"
+
+print(f"The variable x is type: {type(x)}")
+print(f"The variable y is type: {type(y)}" )
