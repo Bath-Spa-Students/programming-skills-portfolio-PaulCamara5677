b25fed83ec083063a595397c7e384b91cc5b693c
PaulCamara5677 <144330112+PaulCamara5677@users.noreply.github.com>
2023-11-15 17:35:55 +0400

    Chapter5-Exercise2-practice-scrapped-idea

---
+++ Chapter 5- Dictionaries/Exercises/Exercise2-scrapped.py
@@ -0,0 +1,131 @@
+#Creating a Glossary list
+
+#keyword variable 
+
+dictionary = {'keyword' : 'variable',
+               'pronunciation' : 'veuh ree uh bl', 
+              'synonyms' : 'changeable, inconstant, editable, differed',
+              'definition' : 'Python variables contains the value of the stored data.' }
+print( dictionary.get ("keyword") )
+
+dictionary = {'keyword' : 'variable',
+              'pronunciation' : 'vahr yuh bel', 
+              'synonyms' : 'changeable, inconstant, editable, differed',
+              'definition' : 'Python variables contains the value of the stored data.' }
+print( dictionary.get ("pronunciation") )
+
+dictionary = {'keyword' : 'variable',
+              'pronunciation' : 'vahr yuh bel', 
+              'synonyms' : 'changeable, inconstant, editable, differed',
+              'definition' : 'Python variables contains the value of the stored data.' }
+print( dictionary.get ("synonyms") )
+
+dictionary = {'keyword' : 'variable',
+              'pronunciation' : 'vahr yuh bel', 
+              'synonyms' : 'changeable, inconstant, editable, differed',
+              'definition' : 'Python variables contains the value of the stored data.' }
+print( dictionary.get ("definition") )
+
+#keyword tuple
+
+dictionary = {'keyword' : 'tuple',
+              'pronunciation' : 'tuh pl', 
+              'synonyms' : 'record, row, list, attribute',
+              'definition' : 'Python tuples are a type of data structure that is very similar to lists.' }
+print( dictionary.get ("keyword") )
+
+dictionary = {'keyword' : 'tuple',
+              'pronunciation' : 'tuh pl', 
+              'synonyms' : 'record, row, list, attribute',
+              'definition' : 'Python tuples are a type of data structure that is very similar to lists.' }
+print( dictionary.get ("pronunciation") )
+
+dictionary = {'keyword' : 'tuple',
+              'pronunciation' : 'tuh pl', 
+              'synonyms' : 'record, row, list, attribute',
+              'explanation' : 'Python tuples are a type of data structure that is very similar to lists.' }
+print( dictionary.get ("synonyms") )
+
+dictionary = {'keyword' : 'tuple',
+              'pronunciation' : 'tuh pl', 
+              'synonyms' : 'record, row, list, attribute',
+              'explanation' : 'Python tuples are a type of data structure that is very similar to lists.' }
+print( dictionary.get ("definition") )
+
+#keyword operator
+
+dictionary = {'keyword' : 'Operator',
+              'pronunciation' : 'o puh ray tuh',
+              'synonyms' : 'director, manager, contractor, user', 
+              'definition' : 'An operator in Python is used to define variables, functions, and classes.' }
+print( dictionary.get ("keyword") )
+
+dictionary = {'keyword' : 'Operator',
+              'pronunciation' : 'o puh ray tuh',
+              'synonyms' : 'director, manager, contractor, user', 
+              'definition' : 'An operator in Python is used to define variables, functions, and classes.' }
+print( dictionary.get ("pronunciation") )
+
+dictionary = {'keyword' : 'Operator',
+              'pronunciation' : 'o puh ray tuh',
+              'synonyms' : 'director, manager, contractor, user', 
+              'definition' : 'An operator in Python is used to define variables, functions, and classes.' }
+print( dictionary.get ("synonyms") )
+
+dictionary = {'keyword' : 'Operator',
+              'pronunciation' : 'o puh ray tuh',
+              'synonyms' : 'director, manager, contractor, user', 
+              'definition' : 'An operator in Python is used to define variables, functions, and classes.' }
+print( dictionary.get ("definition") )
+
+#keyword repository
+
+dictionary = {'keyword' : 'repository',
+              'pronunciation' : 'ruh po zuh tuh ree',
+              'synonyms' : 'archive, storehouse, safe, depot', 
+              'definition' : 'Python repositories are where software development teams that develop Python share their code artifacts.' }
+print( dictionary.get ("keyword") )
+
+dictionary = {'keyword' : 'repository',
+              'pronunciation' : 'ruh po zuh tuh ree',
+              'synonyms' : 'archive, storehouse, safe, depot', 
+              'definition' : 'Python repositories are where software development teams that develop Python share their code artifacts.' }
+print( dictionary.get ("pronunciation") )
+
+dictionary = {'keyword' : 'repository',
+              'pronunciation' : 'ruh po zuh tuh ree',
+              'synonyms' : 'archive, storehouse, safe, depot', 
+              'definition' : 'Python repositories are where software development teams that develop Python share their code artifacts.' }
+print( dictionary.get ("synonyms") )
+
+dictionary = {'keyword' : 'repository',
+              'pronunciation' : 'ruh po zuh tuh ree',
+              'synonyms' : 'archive, storehouse, safe, depot', 
+              'definition' : 'Python repositories are where software development teams that develop Python share their code artifacts.' }
+print( dictionary.get ("definition") )
+
+#keyword commit
+
+dictionary = {'keyword' : 'commit',
+              'pronunciation' : 'kuh mit',
+              'synonyms' : 'execute, fulfill, accomplish, send', 
+              'definition' : 'This method lets an operator save all the changes made in a database.' }
+print( dictionary.get ("keyword") )
+
+dictionary = {'keyword' : 'commit',
+              'pronunciation' : 'kuh mit',
+              'synonyms' : 'execute, fulfill, accomplish, send', 
+              'definition' : 'This method lets an operator save all the changes made in a database.' }
+print( dictionary.get ("pronunciation") )
+
+dictionary = {'keyword' : 'commit',
+              'pronunciation' : 'kuh mit',
+              'synonyms' : 'execute, fulfill, accomplish, send', 
+              'definition' : 'This method lets an operator save all the changes made in a database.' }
+print( dictionary.get ("synonyms") )
+
+dictionary = {'keyword' : 'commit',
+              'pronunciation' : 'kuh mit',
+              'synonyms' : 'execute, fulfill, accomplish, send', 
+              'definition' : 'This method lets an operator save all the changes made in a database.' }
+print( dictionary.get ("definition") )
