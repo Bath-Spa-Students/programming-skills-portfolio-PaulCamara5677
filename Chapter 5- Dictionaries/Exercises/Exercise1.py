46c0f17a194f60decd6265ee7aeb12d72a50fe2c
PaulCamara5677 <144330112+PaulCamara5677@users.noreply.github.com>
2023-11-15 17:39:34 +0400

    Ch5-Exercise1.py

--- Chapter 5- Dictionaries/Exercises/Exercise1.py
+++ Chapter 5- Dictionaries/Exercises/Exercise1.py
@@ -1,20 +1,26 @@
+#Declaring a dictionary.
 dictionary = {'first_name' : 'Paul Yram',
               'last_name' : 'Camara',
               'age' : '21',
               'city' : 'Al Nahda, Sharjah' }
 print( dictionary.get ("first_name") )
-
+#The dictionary above will print out the student's first_name.
+#while the dictionary below will print out the student's last_name.
 dictionary = {'first_name' : 'Paul Yram',
               'last_name' : 'Camara',
               'age' : '21',
               'city' : 'Al Nahda, Sharjah' }
 print( dictionary.get ("last_name") )
 
+#The third dictionary will print out the student's age.
+
 dictionary = {'first_name' : 'Paul Yram',
               'last_name' : 'Camara',
               'age' : '21',
               'city' : 'Al Nahda, Sharjah' }
 print( dictionary.get ("age") )
+
+#The final dictionary will print out where the student currently resides in.
 
 dictionary = {'first_name' : 'Paul Yram',
               'last_name' : 'Camara',
