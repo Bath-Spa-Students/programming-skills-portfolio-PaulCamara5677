94d064280c06ebc8306c48dceeba94b290a18e3f
PaulCamara5677 <144330112+PaulCamara5677@users.noreply.github.com>
2023-11-15 17:07:01 +0400

    Chapter7-Ex5.py

--- Chapter 7- Functions/Exercise5.py
+++ Chapter 7- Functions/Exercise5.py
@@ -3,7 +3,7 @@ def describe_city(city, country = ' UAE'):
 #print will display a string in between the city and country value in order to make it clear where is where.
     print(city + "is located in" + country)
 #Three examples as stated below
-describe_city('Cabanatuan City', ' Philippines')
+describe_city('Cabanatuan City ', ' Philippines')
 #Cabanatuan city is located in Philippines
 describe_city('Dubai ')
 #Dubai is located in UAE
