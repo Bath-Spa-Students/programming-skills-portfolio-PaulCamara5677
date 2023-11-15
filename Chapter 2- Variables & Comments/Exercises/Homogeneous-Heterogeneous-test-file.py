ac2ceea8b2801d8d9983d7f0d658ea027da93bcb
PaulCamara5677 <144330112+PaulCamara5677@users.noreply.github.com>
2023-11-15 17:10:02 +0400

    Ch2-variable-practice.py

---
+++ Chapter 2- Variables & Comments/Exercises/Homogeneous-Heterogeneous-test-file.py
@@ -0,0 +1,25 @@
+# Homogeneous List / Python Supports Heterogeneous List other than C, Java
+
+names = ["Paul Yram", "Sahara","Free time"]
+print (names)
+
+soft_drinks = ("Barbican","Fanta","Pepsi","Mountain Dew","Coca-Cola")
+print(soft_drinks)
+
+
+#Indexxing
+numbers = [66,67,68,69,70]
+print (numbers[-2])
+
+#Lens Function
+Swords =["Rapier","Katana","Saber","Machete","Claymore"]
+print("numbers of Swords in this list: ",(Swords))
+
+#Mutability
+numbers = [74,75,76,77,78]
+numbers[0]=1
+numbers[1]=9
+print(numbers)
+
+#List Slicing
+new_list = [1,2,3,10,15,6,7] 
