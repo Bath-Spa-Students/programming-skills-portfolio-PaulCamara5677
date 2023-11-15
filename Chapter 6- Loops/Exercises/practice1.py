75e49b2042b12e6a420ba4e61446d2c76a64f4c4
PaulCamara5677 <144330112+PaulCamara5677@users.noreply.github.com>
2023-11-15 21:29:07 +0400

    Ch6-practice1.py

--- Chapter 6- Loops/Exercises/practice1.py
+++ Chapter 6- Loops/Exercises/practice1.py
@@ -1,11 +1,6 @@
-#Writing a Python program that use a while loop in order to print even numbers starting from 10 and finishin in 100.
 
-#The variable value 2 will be the key for the code to run successfully.
 
-num = 10
-
-#the code below will run the loop in order.
-
-while num <= 100:
+num = 2
+while num <= 10:
     print(num)
-    num+=10
+    num += 2
